69dd62b27a97df773c6f59d20ac98a3b5c413d83
mohsen yarahmad <mohsenyarahmad1380@gmail.com>
2026-09-19 16:13:42 +0330

    install react,tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-cyan-50">
+        Hello, Vite + React!
+      </h1>
+    </>
+  )
+}
+
+export default App
